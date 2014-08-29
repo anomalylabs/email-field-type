d6fa6245789b8c0bf7d9b413ae4ddbd5169d3a37
Ryan Thompson <service@aiwebsystems.com>
2014-08-29 16:18:47 -0500

    Move towards the standard.

--- src/EmailFieldType.php
+++ src/EmailFieldType.php
@@ -10,47 +10,4 @@ class EmailFieldType extends FieldTypeAbstract
      * @var string
      */
     public $columnType = 'string';
-
-    /**
-     * Field type version
-     *
-     * @var string
-     */
-    public $version = '1.1.0';
-
-    /**
-     * Initial field type validation requirements.
-     *
-     * @var array
-     */
-    public $validation = array(
-        'email'
-    );
-
-    /**
-     * Field type author information.
-     *
-     * @var array
-     */
-    public $author = array(
-        'name' => 'AI Web Systems, Inc.',
-        'url'  => 'http://aiwebsystems.com/',
-    );
-
-    /**
-     * Return the input used for forms.
-     *
-     * @return mixed
-     */
-    public function formInput()
-    {
-        return \Form::input(
-            'text',
-            $this->formSlug,
-            $this->value,
-            array(
-                'placeholder' => $this->getPlacehoder()
-            )
-        );
-    }
 }
