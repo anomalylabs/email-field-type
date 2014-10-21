fb7e4e976274833e80ea04fbee1ef84c3f0a4ac9
Ryan Thompson <service@aiwebsystems.com>
2014-10-21 08:37:56 -0500

    Fixing

--- src/EmailFieldType.php
+++ src/EmailFieldType.php
@@ -1,13 +1,7 @@
-<?php namespace Anomaly\Streams\FieldType\Email;
+<?php namespace Anomaly\Streams\Addon\FieldType\Email;
 
-use Streams\Core\Addon\FieldTypeAbstract;
+use Anomaly\Streams\Platform\Addon\FieldType\FieldTypeAddon;
 
-class EmailFieldType extends FieldTypeAbstract
+class EmailFieldType extends FieldTypeAddon
 {
-    /**
-     * The database column type this field type uses.
-     *
-     * @var string
-     */
-    public $columnType = 'string';
 }
