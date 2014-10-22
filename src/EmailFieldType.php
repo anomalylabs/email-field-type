6ecb99a5972f7a820664faa4ae69fcff0638dee5
Ryan Thompson <service@aiwebsystems.com>
2014-10-22 13:31:51 -0500

    Clean up before final

--- src/EmailFieldType.php
+++ src/EmailFieldType.php
@@ -4,12 +4,4 @@ use Anomaly\Streams\Platform\Addon\FieldType\FieldTypeAddon;
 
 class EmailFieldType extends FieldTypeAddon
 {
-    protected $slug = 'email';
-
-    /**
-     * The database column type this field type uses.
-     *
-     * @var string
-     */
-    public $columnType = 'string';
 }
