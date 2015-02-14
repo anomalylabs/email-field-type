1fba2216dfde2d913e562486f41a68ec69ed22a5
Ryan Thompson <service@aiwebsystems.com>
2015-02-14 13:50:33 -0600

    Config

---
+++ resources/config/config.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'placeholder' => [
+        'type' => 'anomaly.field_type.text'
+    ]
+];
---
+++ resources/lang/en/config.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'placeholder' => [
+        'label' => 'Placeholder'
+    ]
+];
