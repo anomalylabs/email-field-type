65117ad78943966b615d3a3c9e4decd1f399a6fc
Ryan Thompson <service@aiwebsystems.com>
2019-09-14 02:27:20 -0500

    Progress

---
+++ resources/views/input.blade.php
@@ -0,0 +1,4 @@
+<input {!! html_attributes($fieldType->attributes([
+    'type' => 'email',
+    'class' => 'shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline',
+])) !!}>
