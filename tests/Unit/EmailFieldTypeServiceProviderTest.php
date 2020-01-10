69235f98f22dc786a8228b08a95d44cf761a16f5
Ryan Thompson <ryan@pyrocms.com>
2020-01-09 22:46:10 -0600

    Testing

--- tests/Unit/EmailFieldTypeServiceProviderTest.php
+++ tests/Unit/EmailFieldTypeServiceProviderTest.php
@@ -6,9 +6,9 @@ use Anomaly\EmailFieldType\EmailFieldTypeServiceProvider;
 class EmailFieldTypeServiceProviderTest extends TestCase
 {
 
-    public function testProvidesEmailFieldType()
+    public function testProvides()
     {
-        $provider = app(EmailFieldTypeServiceProvider::class);
+        $provider = app(EmailFieldTypeServiceProvider::class, ['app' => app()]);
 
         $this->assertTrue(in_array(EmailFieldType::class, $provider->provides()));
         $this->assertTrue(in_array('anomaly.field_type.email', $provider->provides()));
