10018a5e12c6f6407cd02b374ac1182488537a67
Ryan Thompson <ryan@pyrocms.com>
2020-04-08 22:56:47 -0500

    Vuetify

--- resources/views/input.blade.php
+++ resources/views/input.blade.php
@@ -1,3 +1,24 @@
-<input {!! html_attributes($fieldType->attributes([
-    'type' => 'email',
-])) !!}>
+<v-input>
+    <v-text-field
+    {!! html_attributes([
+        'type' => 'email',
+        'value' => $fieldType->getValue(),
+        'label' => $fieldType->getLabel(),
+        'name'  => $fieldType->getInputName(),
+        'hint'  =>  $fieldType->getInstructions(),
+        
+        'readonly' => $fieldType->isReadonly() ? true : false,
+        'disabled' => $fieldType->isDisabled() ? true : false,
+    
+        // 'data-field'      => $fieldType->getField(),
+        // 'data-provides'   => $fieldType->getNamespace(),
+        // 'data-field_name' => $fieldType->getFieldName(),
+    
+        // 'class' => $fieldType->getClass(),
+        // 'id'    => $fieldType->getInputName(),
+    ]) !!}
+    
+    outlined
+    clearable
+    ></v-text-field>
+</v-input>
